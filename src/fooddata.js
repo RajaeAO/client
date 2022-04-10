45c5c66d7c072b97aeb373e333b7409a484bc650
RajaeAO <shikazukako1393@gmail.com>
2022-04-10 23:31:32 +0000

    first commit

---
+++ src/fooddata.js
@@ -0,0 +1,40 @@
+
+const food = [{
+    "name": "Italian food",
+    "Varients": [
+        "Pasta",
+        "Pizza",
+        "Salade",
+    ],
+    "Prices":[
+        {
+            "Pasta" : 120,
+            "Pizza": 170,
+            "Salade" : 190,
+        }
+    ],
+    "Category": "Burgers",
+    "image": 
+    "Description": "",
+},
+{
+    "name": "Italian food",
+    "Varients": [
+        "small",
+        "medium",
+        "large",
+    ],
+    "Prices":[
+        {
+            "small" : 120,
+            "medium": 170,
+            "large" : 190,
+        }
+    ],
+    "Category": "pasta Italiano",
+    "image": 
+    "Description": "home made pasta prepared with fresh ingredients and topping on your choices",
+},
+},
+],
+export default food
---
+++ src/screens/Homescreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import food from "../fooddata";
+
+export const Homescreen = () => {
+  return (
+    <div>
+      <h1> </h1>
+    </div>
+  );
+};
